f9595c1171247313ed4d5bb6b884979156679986
Yasmine <148632189+yasmins08@users.noreply.github.com>
2023-10-28 21:40:29 +0700

    Create latihan1_variabel.py

---
+++ latihan1_variabel.py
@@ -0,0 +1,39 @@
+a = 10
+b = 5
+print("............")
+print("a:10")
+print("b:5")
+print("..............")
+print((a+b)*(a-b))
+print("..............")
+a = 25
+b = 12
+c = 5
+print("...........")
+print("a:25")
+print("b:12")
+print("c:5")
+print("...........")
+print((a)*(b)/(a+c))
+print("............")
+t = 15
+a = 20
+b = 5
+print("..........")
+print("t:15")
+print("a:20")
+print("b:5")
+print("..........")
+print(1/2*t*(a+b))
+r = 14
+print("........")
+print("r:14")
+print("........")
+print(2*22/7*r)
+print("........")
+r = 14
+print("........")
+print("r:14")
+print("........")
+print(22/7*r*r)
+print("........")
